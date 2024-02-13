ffc545b220607974bcb25711686310db55f47d96
RoxanaTomoiaga <151458429+RoxanaTomoiaga@users.noreply.github.com>
2024-02-13 11:46:58 -0500

    Values for Second Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,1 @@
-let pokemonList = [{ name:"Butterfree", height: "1.1", type: ["bug","safeguard"]},{name: "Mew"},{name: "Meganium"} ];
+let pokemonList = [{ name:"Butterfree", height: "1.1", type: ["bug","safeguard"]},{name: "Mew", height: "0.4", type: ["cat","savage"]},{name: "Meganium"} ];
