473f41a63c708cc3960aa426ecb8a0a510155b5b
RoxanaTomoiaga <151458429+RoxanaTomoiaga@users.noreply.github.com>
2024-02-25 09:26:04 -0500

    arranged objects inside the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-let pokemonList = [{ name:"Butterfree", height: "1.1", type: ["bug","safeguard"]},{name: "Mew", height: "0.4", type: ["cat","savage"]},{name: "Meganium", height: "1.8", type: ["monster","grass"]} ];
+let pokemonList = [
+    {name:"Butterfree", height: "1.1", type: ["bug","safeguard"]},
+    {name: "Mew", height: "0.4", type: ["cat","savage"]},
+    {name: "Meganium", height: "1.8", type: ["monster","grass"]} 
+];
