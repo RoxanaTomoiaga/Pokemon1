ed00d8e38bda56298cca06e98a76743a5f48391a
Roxana Tomoiaga <roxanatomoiaga@yahoo.com>
2024-04-04 12:49:46 -0400

    changed from for loop to forEach() loop

--- js/scripts.js
+++ js/scripts.js
@@ -10,14 +10,28 @@ document.write('Mew', ' (height: 0.4)');
 document.write('<br>');
 document.write('Meganium',' (height: 1.8)');
 
-//loops for my pokemon.
+//change made from for loops to forEach() loop.
 
-for (let i=0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 1.1 && pokemonList[i].height >= 1.8) {
-        console.log(pokemonList[i].name + ' (Wow, that\'s big!)');
-    } else if (pokemonList[i].height >=1.1){
-        console.log(pokemonList[i].name + ' (It\s a medium pokemon)');
-    } else {
-        console.log(pokemonList[i].name + ' ( It\'s a small pokemon)');
-    }
-}
+pokemonList.forEach(function(pokemon){
+    console.log(pokemon.name + ' is ' + pokemon.height + ' tall ' + ' and is a ' + pokemon.type);
+})
+
+let pokemonRepository = (function () {
+    let pokemonList = [
+         {name:"Butterfree", height: 1.1, type: ["bug","safeguard"]},
+         {name: "Mew", height: 0.4, type: ["cat","savage"]},
+         {name: "Meganium", height: 1.8, type: ["monster","grass"]} 
+    ];
+    return {
+        add: function(pokemon) {
+            pokemonList.push(pokemon);
+        },
+        getAll: function() {
+            return pokemonList;
+        }
+    };
+})();
+
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Mew' });
